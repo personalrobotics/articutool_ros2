803be5df4b186b5f6d9cbeb88a1b44e50f347e82
Jose Jaime <jose33jaime@gmail.com>
2025-03-29 12:40:43 -0700

    Revert "Migrate all generate_launch functions for MoveIt configuration into articutool_moveit.launch.py. Simply using the generate_launch functions directly from the provided read-only implementation lacks the flexibility needed to work with namespaces"
    This reverts commit d30b5e8fa8ecf57cc4c870a26ed15f5c79afd735.

--- articutool_moveit/launch/articutool_moveit.launch.py
+++ articutool_moveit/launch/articutool_moveit.launch.py
@@ -7,7 +7,6 @@ from ament_index_python.packages import get_package_share_directory
 from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_demo_launch
 from launch import LaunchDescription
-from launch_ros.parameter_descriptions import ParameterValue
 from launch.actions import (
     DeclareLaunchArgument,
     IncludeLaunchDescription,
@@ -35,180 +34,14 @@ from moveit_configs_utils.launch_utils import (
     add_debuggable_node,
     DeclareBooleanLaunchArg,
 )
+from moveit_configs_utils.launches import (
+    generate_rsp_launch,
+    generate_move_group_launch,
+    generate_spawn_controllers_launch,
+    generate_static_virtual_joint_tfs_launch,
+    generate_moveit_rviz_launch,
+)
 
-def generate_rsp_launch(moveit_config):
-    """Launch file for robot state publisher (rsp)"""
-
-    ld = LaunchDescription()
-    ld.add_action(DeclareLaunchArgument("publish_frequency", default_value="15.0"))
-
-    # Given the published joint states, publish tf for the robot links and the robot description
-    rsp_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        respawn=True,
-        output="screen",
-        parameters=[
-            moveit_config.robot_description,
-            {
-                "publish_frequency": LaunchConfiguration("publish_frequency"),
-            },
-        ],
-    )
-    ld.add_action(rsp_node)
-
-    return ld
-
-def generate_moveit_rviz_launch(moveit_config):
-    """Launch file for rviz"""
-    ld = LaunchDescription()
-
-    ld.add_action(DeclareBooleanLaunchArg("debug", default_value=False))
-    ld.add_action(
-        DeclareLaunchArgument(
-            "rviz_config",
-            default_value=str(moveit_config.package_path / "config/moveit.rviz"),
-        )
-    )
-
-    rviz_parameters = [
-        moveit_config.planning_pipelines,
-        moveit_config.robot_description_kinematics,
-    ]
-
-    add_debuggable_node(
-        ld,
-        package="rviz2",
-        executable="rviz2",
-        output="log",
-        respawn=False,
-        arguments=["-d", LaunchConfiguration("rviz_config")],
-        parameters=rviz_parameters,
-    )
-
-    return ld
-
-def generate_setup_assistant_launch(moveit_config):
-    """Launch file for MoveIt Setup Assistant"""
-    ld = LaunchDescription()
-
-    ld.add_action(DeclareBooleanLaunchArg("debug", default_value=False))
-    add_debuggable_node(
-        ld,
-        package="moveit_setup_assistant",
-        executable="moveit_setup_assistant",
-        arguments=[["--config_pkg=", str(moveit_config.package_path)]],
-    )
-
-    return ld
-
-def generate_static_virtual_joint_tfs_launch(moveit_config):
-    ld = LaunchDescription()
-
-    name_counter = 0
-
-    for key, xml_contents in moveit_config.robot_description_semantic.items():
-        srdf = SRDF.from_xml_string(xml_contents)
-        for vj in srdf.virtual_joints:
-            ld.add_action(
-                Node(
-                    package="tf2_ros",
-                    executable="static_transform_publisher",
-                    name=f"static_transform_publisher{name_counter}",
-                    output="log",
-                    arguments=[
-                        "--frame-id",
-                        vj.parent_frame,
-                        "--child-frame-id",
-                        vj.child_link,
-                    ],
-                )
-            )
-            name_counter += 1
-    return ld
-
-def generate_spawn_controllers_launch(moveit_config):
-    controller_names = moveit_config.trajectory_execution.get(
-        "moveit_simple_controller_manager", {}
-    ).get("controller_names", [])
-    ld = LaunchDescription()
-    for controller in controller_names + ["joint_state_broadcaster"]:
-        ld.add_action(
-            Node(
-                package="controller_manager",
-                executable="spawner",
-                arguments=[controller],
-                output="screen",
-            )
-        )
-    return ld
-
-def generate_move_group_launch(moveit_config):
-    ld = LaunchDescription()
-
-    ld.add_action(DeclareBooleanLaunchArg("debug", default_value=False))
-    ld.add_action(
-        DeclareBooleanLaunchArg("allow_trajectory_execution", default_value=True)
-    )
-    ld.add_action(
-        DeclareBooleanLaunchArg("publish_monitored_planning_scene", default_value=True)
-    )
-    # load non-default MoveGroup capabilities (space separated)
-    ld.add_action(
-        DeclareLaunchArgument(
-            "capabilities",
-            default_value=moveit_config.move_group_capabilities["capabilities"],
-        )
-    )
-    # inhibit these default MoveGroup capabilities (space separated)
-    ld.add_action(
-        DeclareLaunchArgument(
-            "disable_capabilities",
-            default_value=moveit_config.move_group_capabilities["disable_capabilities"],
-        )
-    )
-
-    # do not copy dynamics information from /joint_states to internal robot monitoring
-    # default to false, because almost nothing in move_group relies on this information
-    ld.add_action(DeclareBooleanLaunchArg("monitor_dynamics", default_value=False))
-
-    should_publish = LaunchConfiguration("publish_monitored_planning_scene")
-
-    move_group_configuration = {
-        "publish_robot_description_semantic": True,
-        "allow_trajectory_execution": LaunchConfiguration("allow_trajectory_execution"),
-        # Note: Wrapping the following values is necessary so that the parameter value can be the empty string
-        "capabilities": ParameterValue(
-            LaunchConfiguration("capabilities"), value_type=str
-        ),
-        "disable_capabilities": ParameterValue(
-            LaunchConfiguration("disable_capabilities"), value_type=str
-        ),
-        # Publish the planning scene of the physical robot so that rviz plugin can know actual robot
-        "publish_planning_scene": should_publish,
-        "publish_geometry_updates": should_publish,
-        "publish_state_updates": should_publish,
-        "publish_transforms_updates": should_publish,
-        "monitor_dynamics": False,
-    }
-
-    move_group_params = [
-        moveit_config.to_dict(),
-        move_group_configuration,
-    ]
-
-    add_debuggable_node(
-        ld,
-        package="moveit_ros_move_group",
-        executable="move_group",
-        commands_file=str(moveit_config.package_path / "launch" / "gdb_settings.gdb"),
-        output="screen",
-        parameters=move_group_params,
-        extra_debug_args=["--debug"],
-        # Set the display variable, in case OpenGL code is used internally
-        additional_env={"DISPLAY": os.environ["DISPLAY"]},
-    )
-    return ld
 
 def generate_launch_description():
     # Sim Launch Argument
