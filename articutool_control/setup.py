51d762e728de0f7604141b4791831e64db652cae
Jose Jaime <jose33jaime@gmail.com>
2025-04-16 15:59:05 -0700

    Add service that receives requests to execute joint trajectories

---
+++ articutool_control/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'articutool_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='regulus',
+    maintainer_email='jose33jaime@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'trajectory_execution_server = articutool_control.trajectory_execution_server:main',
+            'send_trajectory_client = articutool_control.send_trajectory_client:main',
+        ],
+    },
+)
