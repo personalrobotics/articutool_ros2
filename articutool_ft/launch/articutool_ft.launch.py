c5b71f4b8cb36613c745201b8ccae49dc836a1fd
Jose Jaime <jose33jaime@gmail.com>
2025-05-26 20:11:19 -0700

    Add launch file for F/T sensor publishing and tare service

---
+++ articutool_ft/launch/articutool_ft.launch.py
@@ -0,0 +1,71 @@
+# articutool_ft/launch/articutool_ft.launch.py
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    """
+    Generates the launch description for the Articutool F/T sensor node.
+    """
+
+    # Declare launch arguments
+    declare_serial_port_arg = DeclareLaunchArgument(
+        "serial_port",
+        default_value="/dev/resense_ft",
+        description="Serial port for the Resense F/T sensor.",
+    )
+
+    declare_publish_rate_arg = DeclareLaunchArgument(
+        "publish_rate_hz",
+        default_value="100.0",  # Example: 100 Hz, adjust as needed
+        description="Rate at which to read sensor and publish data.",
+    )
+
+    declare_wrench_topic_arg = DeclareLaunchArgument(
+        "wrench_topic",
+        default_value="ft_sensor/wrench_raw",  # Changed to be more specific, e.g. not namespaced by node
+        description="Topic to publish WrenchStamped F/T data.",
+    )
+
+    declare_tare_service_arg = DeclareLaunchArgument(
+        "tare_service_name",
+        default_value="ft_sensor/tare",  # Changed to be more specific
+        description="Service name to trigger sensor taring.",
+    )
+
+    declare_sensor_frame_id_arg = DeclareLaunchArgument(
+        "sensor_frame_id",
+        default_value="articutool_ft_sensor_link",  # Example, should match your URDF link
+        description="TF frame ID for the WrenchStamped messages and sensor origin.",
+    )
+
+    # Node configuration
+    resense_ft_node = Node(
+        package="articutool_ft",
+        executable="resense_ft_publisher",  # Matches entry_point in setup.py
+        name="resense_ft_sensor_node",  # Name of the node instance
+        output="screen",
+        parameters=[
+            {
+                "serial_port": LaunchConfiguration("serial_port"),
+                "publish_rate_hz": LaunchConfiguration("publish_rate_hz"),
+                "wrench_topic": LaunchConfiguration("wrench_topic"),
+                "tare_service_name": LaunchConfiguration("tare_service_name"),
+                "sensor_frame_id": LaunchConfiguration("sensor_frame_id"),
+            }
+        ],
+    )
+
+    return LaunchDescription(
+        [
+            declare_serial_port_arg,
+            declare_publish_rate_arg,
+            declare_wrench_topic_arg,
+            declare_tare_service_arg,
+            declare_sensor_frame_id_arg,
+            resense_ft_node,
+        ]
+    )
--- articutool_ft/setup.py
+++ articutool_ft/setup.py
@@ -1,26 +1,31 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
-package_name = 'articutool_ft'
+package_name = "articutool_ft"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*launch.[pxy][yma]*")),
+        ),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='regulus',
-    maintainer_email='jose33jaime@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="regulus",
+    maintainer_email="jose33jaime@gmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'ft_sensor_publisher = articutool_ft.ft_sensor_publisher:main'
+        "console_scripts": [
+            "resense_ft_publisher = articutool_ft.resense_ft_node:main",
         ],
     },
 )
